75d4f2f52cc0f9a2a085ac4a686a504558d11197
jburhan <jimburhan@gmail.com>
2018-08-01 18:38:49 -0700

    bookmark completed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,65 @@
+class BookmarksController < ApplicationController
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new
+    @bookmark.url = params[:bookmark][:url]
+    @topic = Topic.find(params[:topic_id])
+    @bookmark.topic = @topic
+    @bookmark.user = current_user
+
+    if @bookmark.save
+      flash[:notice] = "Bookmark was saved successfully."
+      redirect_to topic_path(@topic)
+    else
+      flash[:error] = "There was an error creating the bookmark. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.assign_attributes(bookmark_params)
+
+    if @bookmark.save
+      flash[:notice] = "Bookmark was updated"
+      redirect_to topic_path(@topic)
+    else
+      flash[:error] = "Error saving bookmark. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+
+    if @bookmark.destroy
+      flash[:notice] = "\"#{@bookmark.url}\" was deleted successfully."
+      redirect_to topic_path(@topic)
+    else
+      flash[:error] = "There was an error deleting the bookmark."
+      render :show
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:url)
+  end
+
+end
